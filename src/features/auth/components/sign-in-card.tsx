8f927c45a3e96f06f2ace4b66be69193f1d10669
Prakhar Kumar <prakhar.meerut@gmail.com>
2025-01-01 16:35:38 +0530

    Authentication flow with convex and google oauth and password based logins
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -3,9 +3,10 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
 import {FcGoogle} from "react-icons/fc";
-import {FaGithub} from "react-icons/fa";
 import {SignInFlow} from "@/features/auth/types";
 import {useState} from "react";
+import {useAuthActions} from "@convex-dev/auth/react";
+import {TriangleAlert} from "lucide-react";
 
 interface SignInCardProps {
     setState: (state: SignInFlow) => void;
@@ -14,6 +15,28 @@ interface SignInCardProps {
 export const SignInCard = ({setState}: SignInCardProps) => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [pending, setPending] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
+
+    const { signIn } = useAuthActions();
+
+    const providerSignIn = (value: "google") => {
+        setPending(true);
+        signIn(value).finally(() => setPending(false));
+    }
+
+    const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        // form submit causes page refresh prevent it!
+        e.preventDefault();
+        setPending(true);
+        signIn("password", {
+            email: email,
+            password: password,
+            flow: "signIn"
+        }).catch(() => {
+            setError("Invalid Email or Password")
+        }).finally(() => setPending(false));
+    }
 
     return (
         <Card className="h-full w-full p-8">
@@ -25,10 +48,16 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                     Use your email or another service to continue
                 </CardDescription>
             </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className={"size-4"}/>
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form className="space-y-2.5" onSubmit={onPasswordSignIn}>
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={email}
                         onChange={(e) => {setEmail(e.target.value)}}
                         placeholder={"Email"}
@@ -36,38 +65,28 @@ export const SignInCard = ({setState}: SignInCardProps) => {
                         required={true}
                     />
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={password}
                         onChange={(e) => {setPassword(e.target.value)}}
                         placeholder={"Password"}
                         type={"password"}
                         required={true}
                     />
-                    <Button type={"submit"} className="w-full" size="lg" disabled={false}>
+                    <Button type={"submit"} className="w-full" size="lg" disabled={pending}>
                         Continue
                     </Button>
                 </form>
                 <Separator />
                 <div className="flex flex-col gap-y-2.5">
                     <Button
-                        disabled={false}
-                        onClick={() => {}}
+                        disabled={pending}
+                        onClick={() => {providerSignIn("google")}}
                         variant={"outline"}
                         size={"lg"}
                         className={"w-full relative"}
                     >
                         <FcGoogle className="size-5 absolute top-3 left-2.5"/>
                         Continue with Google
-                    </Button>
-                    <Button
-                        disabled={false}
-                        onClick={() => {}}
-                        variant={"outline"}
-                        size={"lg"}
-                        className={"w-full relative"}
-                    >
-                        <FaGithub className="size-5 absolute top-3 left-2.5"/>
-                        Continue with Github
                     </Button>
                 </div>
                 <p className="text-xs text-muted-foreground">
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -3,9 +3,10 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
 import {FcGoogle} from "react-icons/fc";
-import {FaGithub} from "react-icons/fa";
 import {SignInFlow} from "@/features/auth/types";
 import {useState} from "react";
+import {useAuthActions} from "@convex-dev/auth/react";
+import {TriangleAlert} from "lucide-react";
 
 interface SignUpCardProps {
     setState: (state: SignInFlow) => void;
@@ -15,6 +16,28 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
+    const [pending, setPending] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
+
+    const { signIn } = useAuthActions();
+
+    const providerSignIn = (value: "google") => {
+        setPending(true);
+        signIn(value).finally(() => setPending(false));
+    }
+
+    const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+        // form submit causes page refresh prevent it!
+        e.preventDefault();
+        setPending(true);
+        signIn("password", {
+            email: email,
+            password: password,
+            flow: "signUp"
+        }).catch(() => {
+            setError("Invalid Email or Password")
+        }).finally(() => setPending(false));
+    }
 
     return (
         <Card className="h-full w-full p-8">
@@ -26,10 +49,16 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
                     Use your email or another service to continue
                 </CardDescription>
             </CardHeader>
+            {!!error && (
+                <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+                    <TriangleAlert className={"size-4"}/>
+                    <p>{error}</p>
+                </div>
+            )}
             <CardContent className="space-y-5 px-0 pb-0">
-                <form className="space-y-2.5">
+                <form className="space-y-2.5" onSubmit={onPasswordSignUp}>
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={email}
                         onChange={(e) => {setEmail(e.target.value)}}
                         placeholder={"Email"}
@@ -37,7 +66,7 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
                         required={true}
                     />
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={password}
                         onChange={(e) => {setPassword(e.target.value)}}
                         placeholder={"Password"}
@@ -45,38 +74,28 @@ export const SignUpCard = ({setState}: SignUpCardProps) => {
                         required={true}
                     />
                     <Input
-                        disabled={false}
+                        disabled={pending}
                         value={confirmPassword}
                         onChange={(e) => {setConfirmPassword(e.target.value)}}
                         placeholder={"Confirm Password"}
                         type={"password"}
                         required={true}
                     />
-                    <Button type={"submit"} className="w-full" size="lg" disabled={false}>
+                    <Button type={"submit"} className="w-full" size="lg" disabled={pending}>
                         Continue
                     </Button>
                 </form>
                 <Separator />
                 <div className="flex flex-col gap-y-2.5">
                     <Button
-                        disabled={false}
-                        onClick={() => {}}
+                        disabled={pending}
+                        onClick={() => {providerSignIn("google")}}
                         variant={"outline"}
                         size={"lg"}
                         className={"w-full relative"}
                     >
                         <FcGoogle className="size-5 absolute top-3 left-2.5"/>
                         Continue with Google
-                    </Button>
-                    <Button
-                        disabled={false}
-                        onClick={() => {}}
-                        variant={"outline"}
-                        size={"lg"}
-                        className={"w-full relative"}
-                    >
-                        <FaGithub className="size-5 absolute top-3 left-2.5"/>
-                        Continue with Github
                     </Button>
                 </div>
                 <p className="text-xs text-muted-foreground">
