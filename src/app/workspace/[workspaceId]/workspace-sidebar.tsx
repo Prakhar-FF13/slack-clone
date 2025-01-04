af81e8e46cbfb08d48ecb331aef6a115cf0091be
Prakhar Kumar <prakhar.meerut@gmail.com>
2025-01-04 16:31:35 +0530

    Showing direct messages section in workspace sidebar
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -6,6 +6,8 @@ import {WorkspaceHeader} from "@/app/workspace/[workspaceId]/workspace-header";
 import {SidebarItem} from "@/app/workspace/[workspaceId]/sidebar-item";
 import {useGetChannels} from "@/features/channels/api/use-get-channels";
 import {WorkspaceSection} from "@/app/workspace/[workspaceId]/workspace-section";
+import {useGetMembers} from "@/features/members/api/use-get-members";
+import {UserItem} from "@/app/workspace/[workspaceId]/user-item";
 
 export const WorkspaceSidebar = () => {
     const workspaceId = useWorkspaceId();
@@ -13,6 +15,7 @@ export const WorkspaceSidebar = () => {
     const {data: member, isLoading: memberLoading} = useCurrentMember({workspaceId});
     const {data: workspace, isLoading: workspaceLoading} = useGetWorkspace({id: workspaceId})
     const {data: channels, isLoading: channelsLoading} = useGetChannels({workspaceId});
+    const {data: members, isLoading: membersLoading} = useGetMembers({workspaceId});
 
     if (workspaceLoading || memberLoading) {
         return <div className={"flex flex-col bg-[#5e2c5f] h-full items-center justify-center"}>
@@ -58,6 +61,21 @@ export const WorkspaceSidebar = () => {
                         />
                     })}
                 </WorkspaceSection>
+            <WorkspaceSection
+                label={"Direct Messages"}
+                hint={"Direct Messages"}
+                onNew={() => {}}
+            >
+                {members?.map((item) => {
+                 return <UserItem
+                    id={item._id}
+                    label={item.user.name}
+                    key={item._id}
+                    image={item.user.image}
+                    // variant={member._id ===}
+                />
+            })}
+            </WorkspaceSection>
         </div>
     )
 }
