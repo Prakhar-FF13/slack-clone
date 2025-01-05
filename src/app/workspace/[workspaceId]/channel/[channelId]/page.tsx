13e03fe1cd38667866ce4f9d4d4b86e6e9334fec
Prakhar Kumar <prakhar.meerut@gmail.com>
2025-01-05 17:37:58 +0530

    Editing and Deleting Channels functionality through header section
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import {useChannelId} from "@/hooks/use-channel-id";
+import {useGetChannelById} from "@/features/channels/api/use-get-channel";
+import {Loader, TriangleAlert} from "lucide-react";
+import {Header} from "@/app/workspace/[workspaceId]/channel/[channelId]/header";
+
 const ChannelIdPage = () => {
+    const channelId = useChannelId();
+
+    const {data: channel, isLoading: channelLoading} = useGetChannelById({id: channelId});
+
+    if (channelLoading) {
+        return (
+            <div className={"h-full flex flex-1 items-center justify-center"}>
+                <Loader className={"animate-spin size-5 text-muted-foreground"}/>
+            </div>
+        )
+    }
+
+    if (!channel) {
+        return (
+            <div className={"h-full flex flex-col gap-y-2 flex-1 items-center justify-center"}>
+                <TriangleAlert className={"size-6 text-muted-foreground"}/>
+                <span className={"text-sm text-muted-foreground"}>
+                    No channels found
+                </span>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            Channel Id page
+        <div className={"flex flex-col h-full"}>
+            <Header title={channel.name}/>
         </div>
     );
 }
