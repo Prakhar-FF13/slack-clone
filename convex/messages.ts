ee56b6eda0dc56d4e624c70df51c7a78c9edb77e
Prakhar Kumar <prakhar.meerut@gmail.com>
2025-01-09 19:34:32 +0530

    conversations schema added.
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- convex/messages.ts
+++ convex/messages.ts
@@ -20,8 +20,7 @@ export const create = mutation({
         workspaceId: v.id("workspaces"),
         channelId: v.optional(v.id("channels")),
         parentMessageId: v.optional(v.id("messages")),
-        // TODO: add converstaion id
-
+        conversationId: v.optional(v.id("conversations")),
     },
     handler: async(ctx,args) => {
         const userId = await getAuthUserId(ctx);
@@ -36,13 +35,24 @@ export const create = mutation({
             throw new Error("Unauthorized")
         }
 
-        // Handle conversation Id
+        let conversationId = args.conversationId;
+
+        if (!args.conversationId && !args.channelId && args.parentMessageId) {
+            const parentMessage = await ctx.db.get(args.parentMessageId);
+
+            if (!parentMessage) {
+                throw new Error("Parent Message not found")
+            }
+
+            conversationId = parentMessage.conversationId
+        }
 
         const messageId = await ctx.db.insert("messages", {
             memberId: member._id,
             body: args.body,
             image: args.image,
             channelId: args.channelId,
+            conversationId: conversationId,
             workspaceId: args.workspaceId,
             parentMessageId: args.parentMessageId,
             updatedAt: Date.now()
